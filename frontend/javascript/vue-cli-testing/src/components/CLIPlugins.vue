88bcc229f92702433eff2727e99cc99a43cf5912
Dusan Turajlic <dusan@turajlic.fi>
2021-04-13 23:57:06 +0300

    Adds a util method for mounting components dependent on suspense

---
+++ frontend/javascript/vue-cli-testing/src/components/CLIPlugins.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>Installed CLI Plugins</h3>
+  <ul>
+    <li>
+      <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+      >babel</a
+      >
+    </li>
+    <li>
+      <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+          target="_blank"
+          rel="noopener"
+      >router</a
+      >
+    </li>
+    <li>
+      <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
+          target="_blank"
+          rel="noopener"
+      >unit-jest</a
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CLIPlugins"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/javascript/vue-cli-testing/tests/unit/example.spec.js
+++ frontend/javascript/vue-cli-testing/tests/unit/example.spec.js
@@ -1,25 +1,12 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 import { useApi } from "@/composables/useApi";
-import { mount } from "@vue/test-utils";
 import flushPromises from "flush-promises";
-import { defineComponent, h, Suspense } from "vue";
+import suspenseShallowMount, {FALLBACK_TEXT} from "./SuspenseShallowMount";
+import HelloWorldSubComponents from "@/components/HelloWorldSubComponents";
 
 jest.mock("@/composables/useApi", () => ({
   useApi: jest.fn(),
 }));
-
-const mountSuspense = (component, props) => {
-  return mount(
-    defineComponent({
-      render() {
-        return h(Suspense, null, {
-          default: h(component, props),
-          fallback: h("div", "fallback"),
-        });
-      },
-    })
-  );
-};
 
 describe("Home.vue", () => {
   useApi.mockResolvedValue({
@@ -30,9 +17,23 @@ describe("Home.vue", () => {
 
   it("renders props.msg when passed", async () => {
     const msg = "new message";
-    const wrapper = mountSuspense(HelloWorld, {
-      msg,
-    });
+    const wrapper = suspenseShallowMount( HelloWorld, { props: {msg} } );
+
+    expect(wrapper.text()).toMatch(FALLBACK_TEXT);
+    expect(wrapper.element).toMatchSnapshot();
+
+    await flushPromises();
+
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.text()).toMatch(msg);
+  });
+
+  it('renders props.message and stubs out all other components', async () => {
+    const msg = "new message";
+    const wrapper = suspenseShallowMount( HelloWorldSubComponents, { props: {msg} } );
+
+    expect(wrapper.text()).toMatch(FALLBACK_TEXT);
+    expect(wrapper.element).toMatchSnapshot();
 
     await flushPromises();
 
